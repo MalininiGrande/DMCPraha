ba2cc677092d0fad6a3e7b7cbf4ed67460586844
Sandra Malinova <90518039+MalininiGrande@users.noreply.github.com>
2021-12-15 18:06:59 +0100

    DMCPraha

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+module.exports = function(eleventyConfig) {
+
+    // Výchozí výstupní složka: _site
+  
+    // Zkopírovat images/ do _site/images
+    eleventyConfig.addPassthroughCopy("images");
+  
+    // Zkopírovat css/ to _site/css/
+    eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.addPassthroughCopy("logos");
+
+    eleventyConfig.addPassthroughCopy("js");
+
+    eleventyConfig.addPassthroughCopy("css_glide");
+
+  
+    return {
+      // možné formáty šablon
+      templateFormats: ["njk", "html", "md", "liquid"],
+  
+      // jako šablonovací jazyk zvolíme Nunjucks
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      dataTemplateEngine: "njk",
+    }
+  
+  };
+  
+  
